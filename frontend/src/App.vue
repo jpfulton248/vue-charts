5bac2b1b4546045b1f155f9e1d980f1bce057176
MZ <iammurtaza53@gmail.com>
2022-03-19 11:09:05 +0500

    code now working

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,30 @@
 <template>
-<div id="app" class="container"> 
-  <!-- <Chart /> -->
-  <!-- <RealTimeStaticChart/> -->
-  <ChartLine/>
-  
-</div>
+  <div id="app" class="container">
+    <div class="row">
+      <Chart />
+    </div>
+    <div class="row">
+      <!-- <RealTimeStaticChart /> -->
+    </div>
+    <div class="row">
+      <!-- <ChartLine /> -->
+    </div>
+  </div>
 </template>
 
 <script>
-// import Chart from './components/Chart.vue';
-// import RealTimeStaticChart from './components/RealTimeStaticChart.vue';
-import ChartLine from './components/ChartLine.vue';
+import Chart from "./components/Chart.vue";
+// import RealTimeStaticChart from "./components/RealTimeStaticChart.vue";
+// import ChartLine from "./components/LineChart.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  // Chart,
-  // RealTimeStaticChart,
-  ChartLine
-  }
-}
+    Chart,
+    // RealTimeStaticChart,
+    // ChartLine,
+  },
+};
 </script>
 
 <style>
@@ -29,13 +34,15 @@ export default {
   display: grid;
   gap: 2rem;
 }
+
+.chart-container {
+  background-color: #f5f4f3;
+  border-radius: 0.5rem;
+}
+
 @media (min-width: 1024px) {
   .container {
     grid-template-columns: repeat(3, minmax(0, 1fr));
   }
-}
-.chart-container {  
-  background-color: #f5f4f3;
-  border-radius: 0.5rem;
 }
 </style>
